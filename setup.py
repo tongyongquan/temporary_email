958083ae83616ff7544c2f6ab8fb8e3ecd159d32
tongyongquan <1070969926@qq.com>
2018-10-12 18:22:06 +0800

    setup

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 import os
 from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
-    README = readme.read()
+with open(os.path.join(os.path.dirname(__file__), 'README.rst'), 'rb') as readme:
+    README = readme.read().decode('utf-8')
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
